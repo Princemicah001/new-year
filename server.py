216077c1336f4c7c65f7cdb0834ef4e9020fb821
Micahprince001 <micahprincemicah001@gmail.com>
2025-12-29 06:17:21 +0300

    Final commit

---
+++ server.py
@@ -0,0 +1,16 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, World!') # Serve a simple message
+
+def run(server_class=HTTPServer, handler_class=SimpleHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
